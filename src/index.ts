1551abbcd069458e70afcf30424978aee846a20f
Stefan Probst <stefanprobst@posteo.de>
2021-11-22 10:38:40 +0100

    feat: add util fn

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export function capitalize(str: string): string {
+  const charCode = str.codePointAt(0);
+  if (charCode === undefined) return "";
+  const firstChar = String.fromCodePoint(charCode);
+  return firstChar.toUpperCase() + str.slice(firstChar.length);
+}
